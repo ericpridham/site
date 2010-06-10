fc2d1284b23491d7858aa0a32c3299d5c085a0a2
Eric <eric@imagineapuddle.com>
2010-06-10 01:07:16 -0400

    Added scrub function to env.

--- comp/env.php
+++ comp/env.php
@@ -22,25 +22,121 @@ class EnvironmentComponent extends SiteComponent
     }
   }
 
-  public function get($var, $type = null) 
+  public function get($var, $source = null) 
   {
-    if (!is_null($type)) {
-      if (isset($this->env[$type][$var])) {
-        return $this->env[$type][$var];
+    if (!is_null($source)) {
+      if (isset($this->env[$source][$var])) {
+        return $this->env[$source][$var];
       }
       return null;
     }
 
     $order = array('post', 'get', 'cookie', 'server', 'shell', 'session');
-    foreach ($order as $type) {
-      if (isset($this->env[$type][$var])) {
-        return $this->env[$type][$var];
+    foreach ($order as $source) {
+      if (isset($this->env[$source][$var])) {
+        return $this->env[$source][$var];
       }
     }
     return null;
   }
 
-  public function set($var, $val, $type = 'all')
+  public function scrub($var, $source, $type, $default = null)
+  {
+    $value = $this->get($var, $source);
+
+    switch ($type) {
+      case 'alphanum':
+        $valid = is_string($value) && preg_match('/^[\w]*$/', $value);
+        break;
+
+      case 'number':
+      case 'numeric':
+        $valid = is_numeric($value);
+        break;
+
+      case 'float':
+        $valid = is_float($value);
+        break;
+
+      case 'int':
+      case 'integer':
+        $valid = is_int($value);
+        break;
+
+      case 'bool':
+      case 'boolean':
+        if (is_numeric($value)) {
+          $value = $value != 0;
+        }
+        elseif (is_string($value)) {
+          if (strtolower($value) === 'true') {
+            $value = true;
+          }
+          elseif (strtolower($value) === 'false') {
+            $value = false;
+          }
+        }
+        $valid = is_bool($value);
+        break;
+
+      case 'array':
+        if (!is_array($value)) {
+          if (strlen($value) == 0) {
+            $value = null;
+          }
+          else {
+            // accept comma-delimited values as an array
+            $value = explode(',', $value);
+          }
+        }
+        $valid = is_array($value);
+        break;
+
+      case 'date':
+        $valid = is_string($value) && $value != '' && ($timestamp = @strtotime($value)) !== -1;
+        if ($valid) {
+          // return a consistent format
+          $value = date('Y-m-d', $timestamp);
+        }
+        elseif (!is_null($default) && ($timestamp = @strtotime($default)) !== -1) {
+          $default = date('Y-m-d', $timestamp);
+        }
+        break;
+
+      case 'datetime':
+        $valid = is_string($value) && $value != '' && ($timestamp = @strtotime($value)) !== -1;
+        if ($valid) {
+          // return a consistent format
+          $value = date('Y-m-d H:i:s', $timestamp);
+        }
+        elseif (!is_null($default) && ($timestamp = @strtotime($default)) !== -1) {
+          $default = date('Y-m-d H:i:s', $timestamp);
+        }
+        break;
+
+      case 'url':
+        // note: {} regex delimiters
+        $valid = is_string($value) && preg_match('{^(https?://)?[\w-./]+(\?[\w-&%= ]*)?(#[\w-]+)?$}', $value);
+        break;
+
+      case 'email':
+        $valid = is_string($value) && preg_match('/^\w+@(\w+\.)+(\w+)$/', $value);
+        break;
+
+      default:
+        $valid = false;
+        break;
+    }
+
+    if ($valid) {
+      return $value;
+    }
+    else {
+      return $default;
+    }
+  }
+
+  public function set($var, $val, $source = 'all')
   {
   }
 }
